a26d26e76dc66bc6b9719f238e915f8a905da105
David Chen <chen7david@me.com>
2020-03-23 17:23:36 +0800

    update

--- src/check.js
+++ src/check.js
@@ -1,11 +1,14 @@
+const { dd } = require('funx-js')
+const { jwt } = require('./../config')
 const JWT = require('jsonwebtoken')
+
 module.exports = {
     requireAuth: async (req, res, next) => {
         const { error } = req.tools
-        const token = req.body.authorization || req.headers['x-access-token']
+        const token = req.headers.authorization || req.headers['x-access-token']
         if(!token) return next(error('required', 'token'))
         try {
-            const object = JWT.verify(token.replace('Bearer ', ''))
+            const object = JWT.verify(token.replace('Bearer ', ''), jwt.secret)
         } catch (err) {
             return next(error('invalid', 'token'))
         }
--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,8 @@
 const { dd } = require('funx-js')
-const { mail } = require('./../config')
+const { mail, jwt } = require('./../config')
 const { template } = require('lodash')
 const mailer = require('./mailer')(mail)
-dd(require('./../config'))
+
 module.exports = {
 
     send: async (req, res, next) => {
